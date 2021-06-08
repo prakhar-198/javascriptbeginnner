58054dfdee07e1819a1cd60f6182d032183d429d
prakhar-198 <80689838+prakhar-198@users.noreply.github.com>
2021-06-08 23:44:20 +0530

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -1,140 +1,51 @@
-console.log('hi');
 
-//Challenge 1: Age in Days
-function ageindays(){
-var birthyear=prompt('what year where u born my friend?');
-var ageindays=(2020-birthyear)*365;
-var h1=document.createElement('h2');
-var textanswer=document.createTextNode('Your age in days is  '+" "+ageindays);
-h1.appendChild(textanswer);
-document.getElementById('flexbox').appendChild(h1);
-h1.setAttribute('id','resettext')
-}
+document.querySelector('#add').addEventListener("click",addtext);
+document.querySelector('#remove').addEventListener("click",removetext);
+document.querySelector('#clear').addEventListener("click",cleartext);
 
-
-function reset(){
-    document.getElementById('resettext').remove();
-
-}
-//Challenge 2:Cat Generator
-function catgenerator(){
-    var image=document.createElement('img');
-    image.src="download.jfif";
-    document.getElementById('flexbox-container2').appendChild(image);
+let i=1;
+function addtext(){
+    let task= ">"+" "+document.getElementById('myinput').value;
+    localStorage.setItem(`lastname${i}`, task);
+    let loc= localStorage.getItem("lastname2");
+    console.log(loc);
+    console.log(task);
+    let check=document.createElement('h4');
+    if(i<5){
+    check=`#checklist${i}`;
+    window.stop();
+    document.querySelector(check).textContent=task;
+    document.querySelector(check).style.color="white";
+    document.getElementById('myinput').value=null;
     
-    
-}
-/*//Challenge 3 Stone paper Scissors
-
-function rpsGame(Yourchoice){
-var humanchoice,botchoice;
-
-humanchoice=Yourchoice.id;
-console.log(humanchoice);
-botchoice=numbertoChoice(randtorpsint());
-results=decidewinner(humanchoice,botchoice);//output=[0,1] or [1,0] or [0.5,0.5]
-console.log(results);
-message=finalmessage(results);//output  Returns object like {message:"You Won", color:"Green"}
-rpsfrontend(humanchoice,botchoice,message);
-}
-function randtorpsint(){
-    return Math.floor(Math.random()*3);
-}
-function numbertoChoice(number){
-    return ['stone','paper','scissors'][number];
-}
-function decidewinner(Yourchoice,comuterChoice){
-   var rpsdatabase= 'stone':{
-        'scissors':1,'paper':0,'stone':0.5
-    }
-    'paper':{
-        'scissors':0,'paper':0.5'stone':1
-    }
-    'scissors':{
-        'scissors':0,'paper':0.5'stone':1
-    }  
-
-    var youscore=rpsdatabase[Yourchoice][comuterChoice];
-    var ComputerScore=rpsdatabase[comuterChoice][Yourchoice];
-return[youscore,ComputerScore]
-}
-function finalmessage([youscore,ComputerScore]){
-    if(youscore===0){
-        return{ 'message':"You lost",'color':'red'}
-    }
-    else if(youscore===0.5){
-        return{ 'message':"You Tied",'color':'yellow'}
-    }
-    else{
-        return{ 'message':"You Won",'color':'yellow'}
-    }
-
-        
-    
-}
-function rpsfrontend(humanimagechoice,botimagechoice,finalmessage){ 
-  var imdatabase={'stone':document.getElementById('stone').src;
-  'paper':document.getElementById('paper').src;
-  'scissors':document.getElementById('scissors').src;
-}
-document.getElementById('stone').remove();
-document.getElementById('paper').remove();
-document.getElementById('scissors').remove();
-
-
-}*/
-
-
-
-//Challenge 4 :-change the color of all buttons
-let copybuttons = document.getElementsByTagName("button");
-console.log("copybuttons", copybuttons);
-
-   
-    let all_buttons = Array.from(copybuttons);
-    console.log("all_buttons", all_buttons);
-
-
-console.log(all_buttons);
-
-function buttoncolorchange(buttonthingy){
-    if(buttonthingy.value==='red'){
-        buttonsred();
-    }else if(buttonthingy.value==='green'){
-        buttonsgreen();
-
-    }else if(buttonthingy.value==='yellow'){
-        buttonsyellow();
-    }else if(buttonthingy.value==='resetcolors'){
-        buttonsreset();
-
-    }else if(buttonthingy.value==='random'){
-        buttonsrandom();
-    }
-
-} function buttonsred(){
-    for(let j=0;j<copybuttons.length;j++){
-        copybuttons[j].classList.replace(copybuttons[j].className,'reset');
-    }
-}
-function buttonsyellow(){
-    for(let j=0;j<copybuttons.length;j++){
-        copybuttons[j].classList.replace(copybuttons[j].className,'btnbtn-warning');
-    }
-}function buttonsgreen(){
-    for(let j=0;j<copybuttons.length;j++){
-        copybuttons[j].classList.replace(copybuttons[j].className,'btnbtn-success');
-    }
-}/*function buttonsreset(){
-    for(let j=0;j<copybuttons.length;j++){
-        copybuttons[j].classList.replace(copybuttons[j].className,all_buttons[j].className);
-       
+    }else if(i=5){
+        check=`#checklist${i}`;
+        document.querySelector(check).textContent=task;
+        document.querySelector(check).style.color="white";
+        document.getElementById('myinput').value=null;
+        i=0;
     }
     
-}*/
-function buttonsrandom(){
-    var choices=['reset','btnbtn-danger','btnbtn-success','btnbtn-warning','clickme'];
-    for(let i=0;i<copybuttons.length;i++){
-    copybuttons[i].classList.replace(copybuttons[i].className,choices[Math.floor(Math.random()*5)]);
+     i++;
+     
+     
+}
+    function removetext(){
+     for(let j=1;j<6;j++){   
+    let cbox=document.getElementById(`checklist${j}`);
+     let uncheck= document.getElementById(`${j}`);
+     if(uncheck.checked){
+         cbox.textContent=null;
+         uncheck.checked=false;
+     }
     }
+}
+function cleartext(){
+    for(let j=1;j<6;j++){   
+        let cbox2=document.getElementById(`checklist${j}`);
+         let uncheck2= document.getElementById(`${j}`);
+             cbox2.textContent=null;
+             uncheck2.checked=false;
+         
+        }
 }
